154bd00605bf43d985edc1382dbb04694875aecc
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-09-17 11:17:49 -0400

    imported utils module from marwynnbot

---
+++ utils/customerrors.py
@@ -0,0 +1,146 @@
+import discord
+from discord.ext import commands
+
+
+
+class CannotPaginate(commands.CommandError):
+    """Error raised when the paginator cannot paginate
+
+    Args:
+        message (str): message that will be sent in traceback
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+
+class PremiumError(commands.CommandError):
+    pass
+
+
+class NoPremiumGuilds(PremiumError):
+    """Error raised when there are no guilds that are MarwynnBot Premium guilds
+    """
+
+    def __init__(self):
+        self.embed = discord.Embed(title="No MarwynnBot Premium Members",
+                                   description="There are no servers registered as MarwynnBot Premium servers \:(",
+                                   color=discord.Color.dark_red())
+
+
+class NoPremiumUsers(PremiumError):
+    """Error raised when the current guild contains no MarwynnBot Premium users
+    """
+
+    def __init__(self):
+        self.embed = discord.Embed(title="No MarwynnBot Premium Members",
+                                   description="This server does not have any MarwynnBot Premium members \:(",
+                                   color=discord.Color.dark_red())
+
+
+class NoGlobalPremiumUsers(NoPremiumUsers):
+    """Error raised when no user is MarwynnBot Premium
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.embed.description = "There are currently MarwynnBot Premium users"
+
+
+class NotPremiumGuild(PremiumError):
+    """Error raised when the current guild is not a MarwynnBot Premium guild
+
+    Args:
+        guild (discord.Guild): the current guild
+    """
+
+    def __init__(self, guild: discord.Guild):
+        self.embed = discord.Embed(title="Not MarwynnBot Premium",
+                                   description=f"This guild, {guild.name}, must have a MarwynnBot Premium Server Subscription"
+                                   " to use this command",
+                                   color=discord.Color.dark_red())
+
+
+class NotPremiumUser(PremiumError):
+    """Error raised when the current user is not a MarwynnBot Premium user
+
+    Args:
+        user (discord.User): the current user
+    """
+
+    def __init__(self, user: discord.User):
+        self.embed = discord.Embed(title="Not MarwynnBot Premium",
+                                   description=f"{user.mention}, you must have a MarwynnBot Premium User Subscription to use this command",
+                                   color=discord.Color.dark_red())
+
+
+class NotPremiumUserOrGuild(PremiumError):
+    """Error raised when the current user and guild are both not MarwynnBot Premium
+
+    Args:
+        user (discord.User): the current user
+        guild (discord.Guild): the current guild
+    """
+
+    def __init__(self, user: discord.User, guild: discord.Guild):
+        self.embed = discord.Embed(title="Not MarwynnBot Premium",
+                                   description=f"{user.mention}, you or this server, {guild.name}, must have a "
+                                   "MarwynnBot Premium Server Subscription to use this command",
+                                   color=discord.Color.dark_red())
+
+
+class UserPremiumException(PremiumError):
+    """Error raised when there is an exception while performing a premium operation on a user
+
+    Args:
+        user (discord.User): the user the error occured with
+    """
+
+    def __init__(self, user: discord.User):
+        self.embed = discord.Embed(title="Set Premium Error",
+                                   description=f"An error occured when trying to operate on {user.display_name}",
+                                   color=discord.Color.dark_red())
+
+
+class UserAlreadyPremium(UserPremiumException):
+    """Error raised when the user already has MarwynnBot Premium
+
+    Args:
+        user (discord.User): the user the error occured with
+    """
+
+    def __init__(self, user: discord.User):
+        super().__init__(user)
+        self.embed.description = f"{user.display_name} already has a MarwynnBot Premium subscription"
+
+
+class GuildPremiumException(PremiumError):
+    """Error raised when there is an exception while performing a premium operation on a guild
+
+    Args:
+        guild (discord.Guild): the guild the error occured with
+    """
+
+    def __init__(self, guild: discord.Guild):
+        self.embed = discord.Embed(title="Set Premium Error",
+                                   description=f"An error occured when trying to operate on {guild.name}",
+                                   color=discord.Color.dark_red())
+
+
+class GuildAlreadyPremium(GuildPremiumException):
+    """Error raised when the guild already has MarwynnBot Premium
+
+    Args:
+        guild (discord.Guild): the guild the error occured with
+    """
+
+    def __init__(self, guild: discord.Guild):
+        super().__init__(guild)
+        self.embed.description = f"{guild.name} already has a MarwynnBot Premium subscription"
+
+
+class MBConnectedError(commands.CommandError):
+    def __init__(self, channel: discord.VoiceChannel):
+        self.embed = discord.Embed(title="MarwynnBot Already Connected",
+                                   description=f"MarwynnBot is already connected to {channel.name}. Please have me join another voice channel",
+                                   color=discord.Color.dark_red())
---
+++ utils/globalcommands.py
@@ -0,0 +1,107 @@
+import json
+import os
+import discord
+import aiohttp
+import asyncio
+import math
+from dotenv import load_dotenv
+from datetime import datetime
+from discord.ext import commands
+from utils import customerrors
+
+load_dotenv()
+env_write = ["TOKEN=YOUR_BOT_TOKEN",
+             "OWNER_ID=YOUR_ID_HERE",
+             "PG_USER=POSTGRES_USERNAME",
+             "PG_PASSWORD=POSTGRES_PASSWORD",
+             "PG_DATABASE=POSTGRES_DATABASE",
+             "PG_HOST=POSTGRES_HOST",
+             "UPDATES_CHANNEL=CHANNEL_ID_HERE",
+             "LAVALINK_IP=IP_ADDR",
+             "LAVALINK_PORT=PORT",
+             "LAVALINK_PASSWORD=DEFAULT_STRING",
+             "CAT_API=API_KEY_FROM_CAT_API",
+             "IMGUR_API=API_KEY_FROM_IMGUR",
+             "REDDIT_CLIENT_ID=CLIENT_ID_FROM_REDDIT_API",
+             "REDDIT_CLIENT_SECRET=CLIENT_SECRET_FROM_REDDIT_API",
+             "USER_AGENT=YOUR_USER_AGENT",
+             "TENOR_API=API_KEY_FROM_TENOR",
+             "GITHUB_TOKEN=PERSONAL_ACCESS_TOKEN"]
+default_env = ["YOUR_BOT_TOKEN",
+               "YOUR_ID_HERE",
+               "POSTGRES_USERNAME",
+               "POSTGRES_PASSWORD",
+               "POSTGRES_DATABASE",
+               "POSTGRES_HOST",
+               "CHANNEL_ID_HERE",
+               "IP_ADDR",
+               "PORT",
+               "DEFAULT_STRING",
+               "API_KEY_FROM_CAT_API",
+               "API_KEY_FROM_IMGUR",
+               "CLIENT_ID_FROM_REDDIT_API",
+               "CLIENT_SECRET_FROM_REDDIT_API",
+               "YOUR_USER_AGENT",
+               "API_KEY_FROM_TENOR",
+               "PERSONAL_ACCESS_TOKEN"]
+
+
+class GlobalCMDS:
+
+    def __init__(self, bot: commands.AutoShardedBot = None):
+        self.version = "v2.0.0-rc.1"
+        self.bot = bot
+        if bot:
+            self.db = self.bot.db
+
+    def init_env(self):
+        if not os.path.exists('.env'):
+            with open('./.env', 'w') as f:
+                f.write("\n".join(env_write))
+                return False
+        return True
+
+    def env_check(self, key: str):
+        if not self.init_env() or os.getenv(key) in default_env:
+            return False
+        return os.getenv(key)
+
+    async def smart_delete(self, message: discord.Message):
+        if message.guild and message.guild.me.guild_permissions.manage_messages:
+            try:
+                await message.delete()
+            except Exception:
+                pass
+
+    async def timeout(self, ctx: commands.Context, title: str, timeout: int) -> discord.Message:
+        embed = discord.Embed(title=f"{title.title()} Timed Out",
+                              description=f"{ctx.author.mention}, your {title} timed out after {timeout} seconds"
+                              " due to inactivity",
+                              color=discord.Color.dark_red())
+        return await ctx.channel.send(embed=embed, delete_after=10)
+
+    async def cancelled(self, ctx: commands.Context, title: str) -> discord.Message:
+        embed = discord.Embed(title=f"{title.title()} Cancelled",
+                              description=f"{ctx.author.mention}, your {title} was cancelled",
+                              color=discord.Color.dark_red())
+        return await ctx.channel.send(embed=embed, delete_after=10)
+
+    async def panel_deleted(self, ctx: commands.Context, title: str) -> discord.Message:
+        embed = discord.Embed(title=f"{title.title()} Cancelled",
+                              description=f"{ctx.author.mention}, your {title} was cancelled because the panel was "
+                              "deleted or could not be found",
+                              color=discord.Color.dark_red())
+        return await ctx.channel.send(embed=embed, delete_after=10)
+
+    async def prefix(self, ctx):
+        if not ctx.guild:
+            return "m?"
+
+        async with self.db.acquire() as con:
+            prefix = await con.fetchval(f"SELECT custom_prefix FROM guild_mb WHERE guild_id = {ctx.guild.id}")
+            return prefix
+
+    @staticmethod
+    def truncate(number: float, decimal_places: int):
+        stepper = 10.0 ** decimal_places
+        return math.trunc(stepper * number) / stepper
